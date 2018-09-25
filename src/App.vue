b87645de2d77343f95e4ac6bc91a595e5168ae73
Darren <491361994@qq.com>
2018-09-25 23:40:46 +0800

    1th Day

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">App
+        <router-view/>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
+
+
